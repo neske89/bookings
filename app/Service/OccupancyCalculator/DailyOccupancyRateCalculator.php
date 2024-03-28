380dbb54e06519c89a15e085b9bb6e3337cb3d5d
neske <mail@nenadmilosavljevic.com>
2024-03-28 12:16:38 +0100

    Add DailyOccupancyRateCalculator service
    A new service called DailyOccupancyRateCalculator has been added. This service extends AbstractOccupancyRateCalculator and is designed to handle the calculation of daily occupancy rates using room capacity data. It includes a new constructor and a calculation function.

---
+++ app/Service/OccupancyCalculator/DailyOccupancyRateCalculator.php
@@ -0,0 +1,23 @@
+<?php
+
+use App\Service\RoomCapacityRetriever;
+use Carbon\Carbon;
+
+class DailyOccupancyRateCalculator extends AbstractOccupancyRateCalculator
+{
+    //generate constructor
+    public function __construct(
+        private RoomCapacityRetriever $roomCapacityRetriever,
+    ) {
+    }
+    public function calculate(Carbon $referenceDateTime, array $roomIds = []): float
+    {
+        $referenceDateTime->copy()->startOfDay();
+        $totalCapacity = $this->roomCapacityRetriever->getDailyCapacity($roomIds);
+        $totalBookings = 0;
+        $totalBlocks = 0;
+        //$sum total bookings
+        //sum total blocks
+        return $this->calculateOccupancy($totalCapacity, $totalBookings, $totalBlocks);
+    }
+}
